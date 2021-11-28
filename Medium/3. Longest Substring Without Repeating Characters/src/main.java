0fc9219d1bf8d306c7bd97ef876206e4dae9f56d
Shlomi <vgtvgy1@gmail.com>
2021-11-28 21:28:07 +0200

    more elegant solution! Works!

--- Medium/3. Longest Substring Without Repeating Characters/src/main.java
+++ Medium/3. Longest Substring Without Repeating Characters/src/main.java
@@ -5,45 +5,45 @@ public class main {
             return 0;
 
         int i_low = 0;
-        int i_high = 0;
+        int i_high = 1;
 
         int max_len = 1;
         int cur_len = 1;
 
-        while(i_high < s.length() - 1) {
-            i_high ++;
+        while(i_high < s.length()) {
             char c_high = s.charAt(i_high);
 
-            boolean is_added = true;
+            //Check if char is not in current segment.
+            boolean in_segment = false;
             for(int i = i_low; i < i_high; i++) {
                 if(c_high == s.charAt(i)) {
-                    //start new segment
-                    max_len = Math.max(max_len, i_high - i_low);
-
-                    i_low ++;
-                    i_high = i_low + 1;
-
-                    is_added = false;
-                    cur_len = 1;
+                    in_segment = true;
                     break;
                 }
             }
-            if(is_added) {
-                cur_len += 1;
+            if(in_segment) {
+                //Start new segment.
+                i_low ++;
+                i_high = i_low + 1;
+                cur_len = 1;
+            } else {
+                i_high++;
+                cur_len++;
+                max_len = Math.max(max_len, cur_len);
             }
         }
-
+        //We do this again, because the segment can begin from start to end.
         max_len = Math.max(max_len, cur_len);
 
         return max_len;
     }
 
     public static void main(String[] args) {
-//        assert(lengthOfLongestSubstring("abcabcbb") == 3);
-//        assert(lengthOfLongestSubstring("bbbbb") == 1);
-//        assert(lengthOfLongestSubstring("pwwkew") == 3);
-//        assert(lengthOfLongestSubstring("") == 0);
-//        assert(lengthOfLongestSubstring("au") == 2);
+        assert(lengthOfLongestSubstring("abcabcbb") == 3);
+        assert(lengthOfLongestSubstring("bbbbb") == 1);
+        assert(lengthOfLongestSubstring("pwwkew") == 3);
+        assert(lengthOfLongestSubstring("") == 0);
+        assert(lengthOfLongestSubstring("au") == 2);
         assert(lengthOfLongestSubstring("dvdf") == 3);
     }
 }
