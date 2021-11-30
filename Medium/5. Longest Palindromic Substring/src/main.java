0b67de10211dd2d5f106ef55ed8298bc372405f6
Shlomi Domnenko <vgtvgy1@gmail.com>
2021-11-30 14:10:31 +0200

    Create main.java

---
+++ Medium/5. Longest Palindromic Substring/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static String longestPalindrome(String s) {
+        return null;
+    }
+    public static void main(String[] args) {
+        assert(longestPalindrome("babad").equals("bab"));
+        assert(longestPalindrome("cbbd").equals("bb"));
+        assert(longestPalindrome("a").equals("a"));
+        assert(longestPalindrome("ac").equals("ac"));
+    }
+}
