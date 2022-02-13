bf6809a4d514594d8274930f54383c3297006fe5
Shlomi <vgtvgy1@gmail.com>
2022-02-13 20:05:42 +0200

    Add Valid Paranthesis solution

---
+++ Easy/20. Valid Parentheses/main.py
@@ -0,0 +1,35 @@
+# (}
+def isValid(s: str) -> bool:
+    if len(s) < 2:
+        return False
+    if len(s) % 2 != 0:
+        return False
+    
+    allowed_pairs = {
+		"{": "}",
+		"[": "]",
+		"(": ")"
+	}
+    
+    stack = []
+    for c in s:
+        # If the character is an opening bracket, push it onto the stack.
+        if c in allowed_pairs.keys():
+            stack.append(c)
+        # If the character is a closing bracket, pop the stack and check if the popped character is the corresponding one.
+        else:
+            if len(stack) == 0:
+                return False
+            head = stack.pop()
+            if allowed_pairs[head] != c:
+                return False
+    return len(stack) == 0
+
+if __name__ == "__main__":
+	assert isValid("()") == True
+	assert isValid(r'()[]{}') == True
+	assert isValid("(]") == False
+	assert isValid("([)]") == False
+	assert isValid("{[]}") == True
+	assert isValid("((") == False
+	assert isValid("){") == False
