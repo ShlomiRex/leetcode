40aea5d0252c6843298ed9e634a9e5312f91ae7c
Shlomi <vgtvgy1@gmail.com>
2022-12-11 18:09:22 +0200

    Complete Medium/921. Minimum Add to Make Parentheses Valid

--- Medium/921. Minimum Add to Make Parentheses Valid/main.py
+++ Medium/921. Minimum Add to Make Parentheses Valid/main.py
@@ -1,5 +1,32 @@
-def minAddToMakeValid(self, s: str) -> int:
-	pass
+def minAddToMakeValid(s: str) -> int:
+	if len(s) == 0:
+		return 0
+	stack = []
+	for c in s:
+		if c == "(":
+			stack.append(c)
+		else:
+			if len(stack) > 0 and stack[-1] == "(":
+				stack.pop()
+			else:
+				stack.append(c)
+	return len(stack)
 
 if __name__ == "__main__":
-	pass
+	s = "())"
+	assert minAddToMakeValid(s) == 1
+	
+	s = "((("
+	assert minAddToMakeValid(s) == 3
+
+	s = "()"
+	assert minAddToMakeValid(s) == 0
+
+	s = "()))"
+	assert minAddToMakeValid(s) == 2
+
+	s = "((()))"
+	assert minAddToMakeValid(s) == 0
+
+	s = "((((((()))"
+	assert minAddToMakeValid(s) == 4
