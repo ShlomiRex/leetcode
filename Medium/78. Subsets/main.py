475f1bf39b1ec6726fb3eeba2c7eddc9298cbf43
Shlomi <vgtvgy1@gmail.com>
2024-04-25 15:20:36 +0300

    Add explanation to 78

--- Medium/78. Subsets/main.py
+++ Medium/78. Subsets/main.py
@@ -2,6 +2,20 @@
 Runtime: 35 ms beats 73%
 Memory: 16 MB beats 31%
 Time taken: 40 minutes
+
+Explanation: similar to BFS, the candidate_start_index is the tree height.
+We start with [], 0 and do for loop, we get:
+
+backtrack([1], 1)
+backtrack([2], 1)
+backtrack([3], 1)
+
+This is the first loop. Now we enter [1],1, the loop will look like this:
+backtrack([1,2], 2)
+backtrack([1,3], 2)
+
+Similarly, we enter [1,2], 1 is:
+backtrack([1,2,3], 3)
 """
 from typing import List
 def subsets(nums: List[int]) -> List[List[int]]:
