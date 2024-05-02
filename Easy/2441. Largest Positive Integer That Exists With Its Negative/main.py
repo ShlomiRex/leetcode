b200897d6bed68927a190f4edaa6bfb703cd9ae7
Shlomi <vgtvgy1@gmail.com>
2024-05-02 15:07:06 +0300

    Add Easy/2441. Largest Positive Integer That Exists With Its Negative

---
+++ Easy/2441. Largest Positive Integer That Exists With Its Negative/main.py
@@ -0,0 +1,14 @@
+from typing import List
+def findMaxK(nums: List[int]) -> int:
+    hashset = set()
+    ans = -1
+    for num in nums:
+        if (-num) in hashset:
+            ans = max(ans, num, -num)
+        hashset.add(num)
+    return ans
+            
+if __name__ == "__main__":
+    assert findMaxK([-1,2,-3,3]) == 3
+    assert findMaxK([-1,10,6,7,-7,1]) == 7
+    assert findMaxK([-10,8,6,7,-2,-3]) == -1
